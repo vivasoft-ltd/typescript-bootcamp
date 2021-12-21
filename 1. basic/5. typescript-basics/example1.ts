ff0e9a2c629540534c80d65c97cf15c7549b7172
Fathe-Mubin <mdfathemubin221b@gmail.com>
2021-12-21 23:21:20 +0600

    basic typescript-basics readme and example (#3)
    Co-authored-by: Fathe <fathe.mubin@vivasoftltd.com>

---
+++ 1. basic/5. typescript-basics/example1.ts
@@ -0,0 +1,8 @@
+const announcement = "Hello World!";
+ 
+// How quickly can you spot the typos?
+announcement.toLocaleLowercase();
+announcement.toLocalLowerCase();
+ 
+// We probably meant to write this...
+announcement.toLocaleLowerCase();
